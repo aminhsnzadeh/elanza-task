3209a7a1ae1a80ad5ba9e9fa6c20b8a2bd5c5a8c
aminhsanzadeh <amin.hsnzadehh@gmail.com>
2025-03-17 16:59:06 +0330

    Added small ui improvement

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
         <div className="stack gap-2 py-8">
             <h3 className="text-2xl font-bold text-center">خوش آمدید</h3>
             <h4 className="text-lg text-center">پنل مدیرت تسک</h4>
-            <p className="text-md text-center mt-8">برای شروع روی <b className="cursor-pointer" onClick={handleGoTasks} >تسک ها</b>  کلیک کنید</p>
+            <p className="text-md text-center mt-8">برای شروع روی <b className="cursor-pointer hover:underline" onClick={handleGoTasks} >تسک ها</b>  کلیک کنید</p>
         </div>
     )
 }
